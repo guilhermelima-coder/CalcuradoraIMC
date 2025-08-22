99178166d2a07ea1e449201781b101eadc952729
Wellington <welltecnic@hotmail.com>
2025-08-22 08:57:06 -0300

    criando a função tabelaIMc

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-
+import Header from './components/Header'
 
 function App() {
   
   return (
-    <>
+    <div className="container">
+      <div className="box">
+        <Header/>
+
+      </div>
       
-    </>
+    </div>
   )
 }
 
